0e36cdbeb78b712bf9cb1420e9dbc9f99f6b7121
Joshua Mills <josh97mills@gmail.com>
2022-02-26 16:10:04 +1100

    added pins loading'

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template,jsonify
-from flask_cors import CORS
 import json
 
 topics = ["dogs","cats","rubbish"]
@@ -7,13 +6,16 @@ topics = ["dogs","cats","rubbish"]
 topicdesc = ["this one is for dogs", 'this page is to report cute animals',"this is for garbage"]
 
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html",nposts = len(topics), topics = topics,topicdesc = topicdesc)
 
-pins = {"lon": 144.42, "lat": -37.9, "message" :"good food"}
+pins = {"posts":[{"lon": 144.98, "lat": -37.8199, "message" :"good food"},
+                 {"lon": 144.91, "lat": -37.8236, "message" :"bad food"},
+                 {"lon": 144.931, "lat": -37.8636, "message" :"good drinks"}]}
+
 @app.route("/get_pins",methods = ["GET"])
 def _pins():
     return pins
@@ -21,10 +23,9 @@ def _pins():
 
 @app.route("/event_post", methods=["POST"])
 def _x():
-    content = request.json
-    print(request)
-    print(request.args)
-    print(json.loads(request.get_data()))
+    added_pin = json.loads(request.get_data())
+    pins["posts"].append(added_pin)
+    print(pins)
     return "POSTED"
 
 app.run(host="0.0.0.0",debug= True)
